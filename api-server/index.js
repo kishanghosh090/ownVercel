4077a7544517e6a0b61c374541c19d0508b171a3
kishanghosh090 <kishanghosh090@gmail.com>
2026-06-17 02:54:29 +0530

    added

---
+++ api-server/index.js
@@ -0,0 +1,72 @@
+import "dotenv/config";
+import express from "express";
+import { generateSlug } from "random-word-slugs";
+import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
+
+const app = express();
+const PORT = process.env.PORT ?? 9000;
+const ecsClient = new ECSClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const config = {
+  CLUSTER: "arn:aws:ecs:ap-south-1:217797467578:cluster/builder-cluster",
+  TASK: "arn:aws:ecs:ap-south-1:217797467578:task-definition/builder-task",
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/project", async (req, res) => {
+  const { githubUrl } = req.body;
+  const projectSlug = generateSlug();
+
+  // spin the container
+
+  const command = new RunTaskCommand({
+    cluster: config.CLUSTER,
+    taskDefinition: config.TASK,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        assignPublicIp: "ENABLED",
+        subnets: [
+          "subnet-02f1b85e465537d7a",
+          "subnet-0b8f2a02fb1a4eecc",
+          "subnet-0629cfdf95b50b6c5",
+        ],
+        securityGroups: ["sg-0cdb7d2e9456cca60"],
+      },
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: "builder-image",
+          environment: [
+            { name: "GIT_REPOSITORY_URL", value: githubUrl },
+            { name: "AWS_ACCESS_KEY_ID", value: "" },
+            {
+              name: "AWS_SECRET_ACCESS_KEY",
+              value: "",
+            },
+            { name: "PROJECT_ID", value: projectSlug },
+          ],
+        },
+      ],
+    },
+  });
+  await ecsClient.send(command);
+  return res.json({
+    status: "queued",
+    data: { projectSlug, url: `http://${projectSlug}.local:8000` },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Api Server is listening at PORT ${PORT}`);
+});
